bdad704e977cb8d2b0ca1251c40c524c1cbb9b6a
EmmanuelUkaegbu <eukaegbu005@gmail.com>
2026-05-24 17:15:47 +0100

    Intial project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import HomeScreen from "./Pages/HomeScreen";
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
